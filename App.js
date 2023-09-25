56910ef2c8f62b4c22431a78fd0698de84489ff2
Haudihou <newasoft@hotmail.com>
2023-09-25 19:12:06 +0300

    Flatlist

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { FlatList, SafeAreaView, StyleSheet, View } from 'react-native';
+import {DATA} from './Data';
+import Row from './Row';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={styles.container}>
+      <View style={styles.centered}>
+      <FlatList
+      data={DATA}
+      renderItem= {({item}) => (
+        <Row person={item} />
+      )}
+    />
+  </View>
+</SafeAreaView>
+);
 }
+
+/*
+function renderItem({item}) {
+  return (<Text>{item.lastname}</Text>);
+}
+*/
+
+/*
+const renderItem = ({item}) => (
+  <Text>{item.lastname}</Text>
+);
+*/
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+  },
+  centered: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
---
+++ Data.js
@@ -0,0 +1,77 @@
+export const DATA = [
+    {
+        id: '1',
+        firstname: 'Juho',
+        lastname: 'Juntunen'
+    },
+    {
+        id: '2',
+        firstname: 'Aki',
+        lastname: 'Ojala'
+    },
+    {
+        id: '3',
+        firstname: 'Joni',
+        lastname: 'Huttunen'
+    },
+    {
+        id: '4',
+        firstname: 'Sonja',
+        lastname: 'Siuvatti'
+    },
+    {
+        id: '5',
+        firstname: 'Jaana',
+        lastname: 'Pajala'
+    },
+    {
+        id: '6',
+        firstname: 'Pekka',
+        lastname: 'Routa'
+    },
+    {
+        id: '7',
+        firstname: 'Arttu',
+        lastname: 'Jauhiainen'
+    },
+    {
+        id: '8',
+        firstname: 'Mauno',
+        lastname: 'Koivisto'
+    },
+    {
+        id: '9',
+        firstname: 'Joonas',
+        lastname: 'Tammi'
+    },
+    {
+        id: '10',
+        firstname: 'Elias',
+        lastname: 'Paananen'
+    },
+    {
+        id: '11',
+        firstname: 'Antti',
+        lastname: 'Soini'
+    },
+    {
+        id: '12',
+        firstname: 'Petteri',
+        lastname: 'Jaakola'
+    },
+    {
+        id: '13',
+        firstname: 'Anna',
+        lastname: 'Perälä'
+    },
+    {
+        id: '14',
+        firstname: 'Topi',
+        lastname: 'Pouta'
+    },
+    {
+        id: '15',
+        firstname: 'Kimmo',
+        lastname: 'Kangas'
+    },
+]
